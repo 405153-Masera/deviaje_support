c2def3e7034acd7b468058faaf8a3c3efe15708c
405153-Masera <405153@tecnicatura.frc.utn.edu.ar>
2025-04-11 09:42:58 -0300

    first commit

---
+++ src/main/java/masera/deviaje_support/DeviajeSupportApplication.java
@@ -0,0 +1,13 @@
+package masera.deviaje_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviajeSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviajeSupportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/masera/deviaje_support/DeviajeSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package masera.deviaje_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviajeSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
